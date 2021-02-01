43e4dd8438352dec5eaecabe4a75a452c214060e
Tobias Rosskopf (privat) <tobirosskopf@gmail.com>
2021-02-01 18:04:51 +0100

    Worked on endpoints; Worked on Authentication

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 
-from .views import current_user, UserList, UserDetail
+from .views import current_user, UserList, UserDetail, signup
 
 
 urlpatterns = [
+    # AllowAny
+    path("signup/", signup),
+
+    # IsAuthenticated
     path("current_user/", current_user),
     path("users/", UserList.as_view()),
     path("users/<str:username>/", UserDetail.as_view()),
--- app/views.py
+++ app/views.py
@@ -1,39 +1,44 @@
 from django.http import Http404
 
 from rest_framework import permissions, status
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, permission_classes
 
 from .serializers import UserSerializer, UserSerializerWithToken
 
 from .models import CustomUser
 
 
+@api_view(["POST"])
+@permission_classes([permissions.AllowAny])
+def signup(request):
+    serializer = UserSerializerWithToken(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
 @api_view(["GET"])
+@permission_classes([permissions.IsAuthenticated])
 def current_user(request):
     serializer = UserSerializer(request.user)
     return Response(serializer.data)
 
-
 class UserList(APIView):
 
-    permission_classes = (permissions.AllowAny,)
+    permission_classes = [permissions.IsAuthenticated]
 
     def get(self, request, format=None):
         users = CustomUser.objects.all()
         serializer = UserSerializer(users, many=True)
         return Response(serializer.data)
 
-    def post(self, request, format=None):
-        serializer = UserSerializerWithToken(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
 
 class UserDetail(APIView):
+
+    permission_classes = [permissions.IsAuthenticated]
 
     def get_object(self, username):
         try:
